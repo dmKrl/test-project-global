a2c707c18098bfbd3833730ac6c0664d225ad70f
Dmitry Kralichkin <dm_krl@mail.ru>
2024-02-03 19:03:18 +1200

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
+import s from "./Footer.module.css";
 
 const Footer = () => {
     return (
-        <div>
-            <div>
-                <div>
-                    <h4>Интерьер</h4>
-                    <Link to="/"></Link>
-                    <Link to="/"></Link>
-                </div>
+        <div className={s.footer}>
+            <div className={s.footerLogo}>
+                <Link to="/">Интерьер</Link>
             </div>
-            <div>
-                <h4>Контакты</h4>
-                <div>
-                    <Link to="/"></Link>
-                    <Link to="/"></Link>
+            <div className={s.footerInfo}>
+                <div className={s.footerItem}>
+                    <h4 className={s.footerHeading}>Меню</h4>
+                    <div className={s.footerNav}>
+                        <Link to="/">Каталог</Link>
+                        <Link to="/backet-page">Корзина</Link>
+                    </div>
+                </div>
+                <div className={s.footerItem}>
+                    <h4 className={s.footerHeading}>Контакты</h4>
+                    <div className={s.footerNav}>
+                        <Link to="+79111111111">+7 908 800 80 80</Link>
+                        <Link to="malito:random_mail@mail.ru">
+                            help@interier.com
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
