29d021480bdd7701f0a1cd76261a09a9e149d79a
Dmitry Kralichkin <dm_krl@mail.ru>
2024-02-05 16:03:07 +1200

    add form validation

--- src/components/form-backet/FormBacket.jsx
+++ src/components/form-backet/FormBacket.jsx
@@ -1,28 +1,63 @@
 import { useSelector } from "react-redux";
 import s from "../../pages/backet-page/BacketPage.module.css";
 import { selectFullPrice } from "../../redux/slices/backetSlice";
+import { useForm } from "react-hook-form";
 
 const FormBacket = () => {
     const fullPrice = useSelector(selectFullPrice);
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm({
+        mode: "onBlur",
+    });
+    function onSubmit(data) {
+        console.log(data);
+    }
     return (
-        <form className={s.form}>
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
             <input
+                {...register("firstName", {
+                    required: "Поле обязательно к заполнению",
+                })}
                 type="text"
                 className={s.formInput}
                 placeholder="Имя Фамилия"
             />
+            <span className={s.messageError}>
+                {errors?.firstName && <p>{errors?.firstName?.message}</p>}
+            </span>
             <input
-                type="text"
+                {...register("phoneNumber", {
+                    required: "Поле обязательно к заполнению",
+                    minLength: {
+                        value: 10,
+                        message: "Минимум 5 символов",
+                    },
+                })}
+                type="number"
                 className={s.formInput}
                 placeholder="+ 7 904 000 80 80"
             />
+            <span className={s.messageError}>
+                {errors?.phoneNumber && <p>{errors?.phoneNumber?.message}</p>}
+            </span>
             <input
+                {...register("address", {
+                    required: "Поле обязательно к заполнению",
+                })}
                 type="text"
                 className={s.formInput}
                 placeholder="Адрес доставки"
             />
+            <span className={s.messageError}>
+                {errors?.address && <p>{errors?.address?.message}</p>}
+            </span>
             <p className={s.formResult}>Итого: {fullPrice} руб.</p>
-            <button className={s.formSubmit}>Оформить заказ</button>
+            <button type="submit" className={s.formSubmit}>
+                Оформить заказ
+            </button>
         </form>
     );
 };
