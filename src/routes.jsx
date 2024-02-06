cbb613daaaaa1de1f519b36f90799b789661763e
Dmitry Kralichkin <dm_krl@mail.ru>
2024-02-06 17:44:25 +1200

    some fix paths in routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,8 +6,8 @@ import CatalogPage from "./pages/catalog-page/CatalogPage";
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route element={<MainPage />} path="https://dmKrl.github.io/test-project-global/">
-                <Route element={<CatalogPage />} path="https://dmKrl.github.io/test-project-global/" />
+            <Route element={<MainPage />} path="/">
+                <Route element={<CatalogPage />} path="/" />
                 <Route element={<BasketPage />} path="/basket-page" />
             </Route>
         </Routes>
